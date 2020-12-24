2ed73c218c74b31f13165ea3ce2fe637a8593df9
Yiorgos Bagakis <ybagakis@gmail.com>
2020-12-24 16:31:23 +0100

    part 1-3 & 1-4

---
+++ part1-4/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  
+  const course = 'Half Stack application development'
+  const parts = [ 
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>{parts[0].name}: {parts[0].exercises}</p>
+      <p>{parts[1].name}: {parts[1].exercises}</p>
+      <p>{parts[2].name}: {parts[2].exercises}</p>
+      <p>Number of exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,59 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = () => { 
-  
-  const course = 'Half Stack application development'
-  
-  return ( 
-  <div>
-    <h1>{course}</h1>
-  </div>
-  );
-}
-
-
-const Content = (props) => {
-
-  const part1 = 'Fundamentals of React'
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
-  
-
-  return (
-    <div>
-      <p>
-        {part1} {props.exercises1}
-      </p>
-      <p>
-        {part2} {props.exercises2}
-      </p>
-      <p>
-        {part3} {props.exercises3}
-      </p>
-      
-    </div>
-  )
-
-}
-
-const Total = (props) => {
-
-  return (
-  <div>
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  </div>
-  )
-  }
-
 const App = () => {
-  
+  const [counter, setCounter] = useState(0)
+  setTimeout(() => setCounter(counter + 1), 60000)
   return (
-    <div>
-      <Header />
-      <Content />
-      <Total exercises1={10} exercises2={7} exercises3={14}/>
-    </div>
+    <div><p>The counter counts:</p><code>{counter}</code></div>
+    
   )
 }
 
