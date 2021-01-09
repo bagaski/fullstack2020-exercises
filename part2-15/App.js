4255b5587e1f2f24392ec05563ae9cdc6fa7527b
Yiorgos Bagakis <ybagakis@gmail.com>
2021-01-09 12:48:00 +0100

    exe part2-15

---
+++ part2-15/App.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Form = (props) => {
+
+    return (
+        <form onSubmit={props.addperson}>
+            <div>
+                name: <input type='text' value={props.newperson} onChange={props.changeperson} />
+            </div>
+            <div>
+                number: <input type='text' value={props.newnumber} onChange={props.changenumber} /></div>
+            <div>
+                <button type='submit'>Add</button>
+            </div>
+        </form>
+    )
+
+}
+
+const App = () => {
+    const [persons, setPersons] = useState([])
+    const [newPerson, setNewPerson] = useState('type a name...')
+    const [newNumber, setNewNumber] = useState('type phonenumber...')
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, [])
+
+
+
+    //console.log('render', persons.length, 'persons')
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        setNewPerson(event.target.value)
+        setNewNumber(event.target.value)
+        const personsObject = {
+            name: newPerson,
+            number: newNumber,
+        }
+        setPersons(persons.concat(personsObject))
+
+        persons.some(person => person.name === newPerson) ? alert(`${newPerson} is already added to phonebook`) : setNewPerson('');
+        setNewNumber('');
+
+        console.log(newPerson, persons)
+
+        axios
+            .post('http://localhost:3001/persons', personsObject)
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(persons.concat(response.data))
+                setNewPerson('');
+            })
+
+    }
+
+    const handlePersonChange = (event) => {
+        setNewPerson(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    const noBullets = {
+        listStyleType: 'none',
+        padding: '0px',
+        margin: '0px'
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Form addperson={addPerson} newperson={newPerson} newnumber={newNumber} changeperson={handlePersonChange} changenumber={handleNumberChange} />
+            <h2>Phone Numbers</h2>
+            <ul style={noBullets}>
+                {persons.map(person => <li key={person.id}><i>{person.name} </i>
+                    {person.number}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default App
